87974cf4abfe4a0305334edf48364dfe8cea8970
--global <--global>
2025-06-13 13:20:10 +0200

    .

--- test/LiveTest_ma1069/livetest2.jl
+++
@@ -1,6 +0,0 @@
-remoteibnfhandler = ibnfs[1].ibnfhandlers[2]
-
-data = Dict("src_domain" => "00000000-0000-0000-0000-000000000003","newstate" => "Compiled","idagnodeid" => "00000000-0000-0000-0000-00000000000c")
-#data = Dict()
-
-resp = MINDF.send_request(remoteibnfhandler, MINDF.HTTPCodes.REMOTEINTENT_STATEUPDATE, data)
