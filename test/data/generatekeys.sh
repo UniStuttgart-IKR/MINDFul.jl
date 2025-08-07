ff90b91a3ca65ff1296ef1a3c7d271617e7b8056
--global <--global>
2025-08-07 20:03:21 +0200

    rsa authentication - testsuite 4

--- test/data/generatekeys.sh
+++ test/data/generatekeys.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 cd test/data
-openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -pkeyopt rsa_keygen_pubexp:3 -out rsa_priv1.pem
-openssl pkey -in rsa_priv1.pem -out rsa_pub1.pem -pubout
-openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -pkeyopt rsa_keygen_pubexp:3 -out rsa_priv2.pem
-openssl pkey -in rsa_priv2.pem -out rsa_pub2.pem -pubout
-openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -pkeyopt rsa_keygen_pubexp:3 -out rsa_priv3.pem
-openssl pkey -in rsa_priv3.pem -out rsa_pub3.pem -pubout
+if [ ! -f rsa_priv1.pem ] || [ ! -f rsa_priv2.pem ] || [ ! -f rsa_priv3.pem ]; then
+    openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -pkeyopt rsa_keygen_pubexp:3 -out rsa_priv1.pem 2>/dev/null
+    openssl pkey -in rsa_priv1.pem -out rsa_pub1.pem -pubout
+    openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -pkeyopt rsa_keygen_pubexp:3 -out rsa_priv2.pem 2>/dev/null
+    openssl pkey -in rsa_priv2.pem -out rsa_pub2.pem -pubout
+    openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -pkeyopt rsa_keygen_pubexp:3 -out rsa_priv3.pem 2>/dev/null
+    openssl pkey -in rsa_priv3.pem -out rsa_pub3.pem -pubout
+fi
